54599ebd7aad686d40c0a813670977bb0d4fa67e
Ahmad Obay <ahmad.obay@gmail.com>
2025-02-25 20:20:40 -0800

    Add Network Out Total metric alert for virtual machine

---
+++ resource.azurerm_monitor_metric_alert.network_out_total.tf
@@ -0,0 +1,23 @@
+resource "azurerm_monitor_metric_alert" "network_out_total" {
+  name                = "Network Out Total - ${var.vmname}"
+  resource_group_name = var.monitoring_resource_group_name
+  scopes              = [var.monitoring_linux_virtual_machine_id]
+  description         = "Network Out Total - ${var.vmname}"
+  severity            = 3
+  frequency           = "PT${var.monitoring_metric_alert_frequency}M"
+  window_size         = "PT${var.monitoring_metric_alert_window_size}M"
+  criteria {
+    metric_namespace = "Microsoft.Compute/virtualMachines"
+    metric_name      = "Network Out Total"
+    aggregation      = "Total"
+    operator         = "GreaterThan"
+    threshold        = 200000000000
+  }
+  action {
+    action_group_id = var.monitoring_action_group_id
+  }
+
+  tags = {
+    alertRuleCreatedWithAlertsRecommendations = "true"
+  }
+}
