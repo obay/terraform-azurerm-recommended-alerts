919050d62bf595b5dae178ed5f205fecc0519a52
Ahmad Obay <ahmad.obay@gmail.com>
2025-02-25 20:20:52 -0800

    Add Percentage CPU metric alert for virtual machine

---
+++ resource.azurerm_monitor_metric_alert.percentage_cpu.tf
@@ -0,0 +1,19 @@
+resource "azurerm_monitor_metric_alert" "percentage_cpu" {
+  name                = "Percentage CPU - ${var.vmname}"
+  resource_group_name = var.monitoring_resource_group_name
+  scopes              = [var.monitoring_linux_virtual_machine_id]
+  description         = "Percentage CPU - ${var.vmname}"
+  severity            = 3
+  frequency           = "PT${var.monitoring_metric_alert_frequency}M"
+  window_size         = "PT${var.monitoring_metric_alert_window_size}M"
+  criteria {
+    metric_namespace = "Microsoft.Compute/virtualMachines"
+    metric_name      = "Percentage CPU"
+    aggregation      = "Average"
+    operator         = "GreaterThan"
+    threshold        = 80
+  }
+  action {
+    action_group_id = var.monitoring_action_group_id
+  }
+}
