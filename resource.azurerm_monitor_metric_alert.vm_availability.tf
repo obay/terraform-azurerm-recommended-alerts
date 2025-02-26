54e54e694a318d2f37c9e70ed71d8fe58d938d61
Ahmad Obay <ahmad.obay@gmail.com>
2025-02-25 20:36:04 -0800

    Add VM Availability metric alert for virtual machine

---
+++ resource.azurerm_monitor_metric_alert.vm_availability.tf
@@ -0,0 +1,19 @@
+resource "azurerm_monitor_metric_alert" "vm_availability" {
+  name                = "VM Availability - ${var.vmname}"
+  resource_group_name = var.monitoring_resource_group_name
+  scopes              = [var.monitoring_linux_virtual_machine_id]
+  description         = "VM Availability - ${var.vmname}"
+  severity            = 3
+  frequency           = "PT${var.monitoring_metric_alert_frequency}M"
+  window_size         = "PT${var.monitoring_metric_alert_window_size}M"
+  criteria {
+    metric_namespace = "Microsoft.Compute/virtualMachines"
+    metric_name      = "VmAvailabilityMetric"
+    aggregation      = "Average"
+    operator         = "LessThan"
+    threshold        = 1
+  }
+  action {
+    action_group_id = var.monitoring_action_group_id
+  }
+}
