d58a339904b2be8c58622ef9fb321b062c440bba
Ahmad Obay <ahmad.obay@gmail.com>
2025-02-25 20:06:43 -0800

    Add OS Disk IOPS Consumed Percentage metric alert

--- resource.azurerm_monitor_metric_alert.os_disk_iops_consumed_percentage.tf
+++ resource.azurerm_monitor_metric_alert.os_disk_iops_consumed_percentage.tf
@@ -1,17 +1,17 @@
-resource "azurerm_monitor_metric_alert" "cpu_usage_alert" {
-  name                = var.monitoring_metric_alert_name
+resource "azurerm_monitor_metric_alert" "os_disk_iops_consumed_percentage" {
+  name                = "OS Disk IOPS Consumed Percentage - ${var.vmname}"
   resource_group_name = var.monitoring_resource_group_name
   scopes              = [var.monitoring_linux_virtual_machine_id]
-  description         = var.monitoring_metric_alert_description
-  severity            = var.monitoring_metric_alert_severity
+  description         = "OS Disk IOPS Consumed Percentage - ${var.vmname}"
+  severity            = 3
   frequency           = "PT${var.monitoring_metric_alert_frequency}M"
   window_size         = "PT${var.monitoring_metric_alert_window_size}M"
   criteria {
     metric_namespace = "Microsoft.Compute/virtualMachines"
-    metric_name      = "Percentage CPU"
+    metric_name      = "OS Disk IOPS Consumed Percentage"
     aggregation      = "Average"
     operator         = "GreaterThan"
-    threshold        = 80
+    threshold        = 95
   }
   action {
     action_group_id = var.monitoring_action_group_id
